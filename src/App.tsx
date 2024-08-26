28f3f2c0c8a8145ee3fd922f1341889cd1c7a57d
varshini <varshini@varshinis-MacBook-Air.local>
2024-08-26 11:06:07 +0530

    ListForm component update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import ListForm from "./components/ListForm";
 const App = () => {
+  interface Countries {
+    id: number;
+    country: string;
+    code: string;
+  }
+
+  let countries: Countries[] = [
+    { id: 1, country: "Iceland", code: "IS" },
+    { id: 2, country: "India", code: "IN" },
+    { id: 3, country: "Indonesia", code: "ID" },
+    { id: 4, country: "Iran, Islamic Republic Of", code: "IR" },
+    { id: 5, country: "Iraq", code: "IQ" },
+    { id: 6, country: "Germany", code: "DE" },
+    { id: 7, country: "Ghana", code: "GH" },
+    { id: 8, country: "Gibraltar", code: "GI" },
+    { id: 9, country: "America", code: "US" },
+  ];
+
   return (
     <div>
-      <ListForm />
+      <ListForm items={countries} heading="Cities" />
     </div>
   );
 };
--- src/components/ListForm.tsx
+++ src/components/ListForm.tsx
@@ -1,23 +1,40 @@
-const ListForm = () => {
-  const countries = [
-    { id: 1, country: "Iceland", code: "IS" },
-    { id: 2, country: "India", code: "IN" },
-    { id: 3, country: "Indonesia", code: "ID" },
-    { id: 4, country: "Iran, Islamic Republic Of", code: "IR" },
-    { id: 5, country: "Iraq", code: "IQ" },
-    { id: 6, country: "Germany", code: "DE" },
-    { id: 7, country: "Ghana", code: "GH" },
-    { id: 8, country: "Gibraltar", code: "GI" },
-    { id: 9, country: "America", code: "US" },
-  ];
+import { useState } from "react";
+
+interface Props {
+  items: { id: number; country: string; code: string }[];
+  heading: string;
+}
+
+const ListForm = ({ items, heading }: Props) => {
+  // countries = [];
+
+  // event handler
+  // const handleClick = (event: MouseEvent) => console.log(event);
+
+  /* HOOKS */
+
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleClick = (index: number) => {
+    setSelectedIndex(index);
+    console.log(`Clicked: ${items[index].country} (Index ${index})`);
+  };
 
   return (
     <>
-      <h1>List Group</h1>
-
+      <h1>{heading}</h1>
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {countries.map((item) => (
-          <li key={item.id}>
+        {items.map((item, index) => (
+          <li
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item.id}
+            onClick={() => handleClick(index)}
+          >
             CountryID: {item.id}, CountryName: {item.country}
           </li>
         ))}
